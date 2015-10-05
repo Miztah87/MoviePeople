31b8084e737ef1e8aba3c30a28909ceeb13738d7
Miztah87 <zalantassaly@gmail.com>
2015-10-05 10:43:40 +0200

    test for shopping cart

---
+++ TestForCompulsory/Models/OrderLine.cs
@@ -0,0 +1,12 @@
+﻿using TestProxy.DomainModel;
+
+namespace TestForCompulsory.Models
+{
+    public class OrderLine
+    {
+        public Movie movie;
+
+        public int Amount { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
