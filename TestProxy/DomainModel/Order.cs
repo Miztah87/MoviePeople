d20fd2601ccf394f112d5bf2a6fb5cececc7f4fc
unknown <kriahja@gmail.com>
2015-10-22 17:20:53 +0200

    Order thingy

---
+++ TestProxy/DomainModel/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProxy.DomainModel
+{
+    [Table("Order")]
+   public class Order
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int UserId { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public int MovieId { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
