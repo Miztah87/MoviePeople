1e2291429d5dbdf606ba3933f8351cf870cb9c01
unknown <zalantassaly@gmail.com>
2015-10-22 15:27:34 +0200

    user

--- TestForCompulsory/Models/AccountViewModels.cs
+++ TestForCompulsory/Models/AccountViewModels.cs
@@ -71,6 +71,14 @@ namespace TestForCompulsory.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+        [Required]
+        public string StreetName { get; set; }
+        [Required]
+        public int StrNr { get; set; }
+        [Required]       
+        public int Zip { get; set; }
+        [Required]
+        public string Country { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
